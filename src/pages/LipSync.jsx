a5e5a8fba46127f7e3247d43fdffc1161fe8dd33
foryou929 <zxcvwert3457@gmail.com>
2024-08-02 21:46:59 +0900

    feat: update-lipsync-ui

--- src/pages/LipSync.jsx
+++ src/pages/LipSync.jsx
@@ -1,6 +1,38 @@
+import { Box, Button, Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import AudioUploader from "../components/AudioUploader";
+import VideoUploader from "../components/VideoUploader";
+import axios from "../utils/axios";
+
 const LipSync = () => {
+    const [audio, setAudio] = useState();
+    const [video, setVideo] = useState();
+    const handleProcess = async () => {
+        const formData = new FormData();
+        formData.append('audio', audio);
+        formData.append('video', video);
+        await axios.post('/process', formData);
+    }
+
     return (
-        <></>
+        <Box h='100%' bg='url("images/background.jpg")'>
+            <Flex mx='auto' p={{ base: 8, lg: 16 }} w='100%' maxW='1440px' h='100%' direction={{ base: 'column', lg: 'row' }} gap={4}>
+                <Flex w='100%' direction='column' gap={4}>
+                    <VideoUploader w='100%' aspectRatio={1.7778} bg='#FFF4' border='2px dashed #888' onChange={setVideo} />
+                    <AudioUploader w='100%' as={Flex} justify='center' bg='#FFF4' border='2px dashed #888' onChange={setAudio} />
+                </Flex>
+                <Flex w='100%' direction='column' gap={4}>
+                    <Box aspectRatio={1.7778} border='2px dashed #888' overflow='hidden'>
+                        <video controls style={{ width: '100%', height: '100%' }} />
+                    </Box>
+                    <Flex justify='right'>
+                        <Button isDisabled={!audio || !video} onClick={handleProcess}>
+                            Process
+                        </Button>
+                    </Flex>
+                </Flex>
+            </Flex>
+        </Box>
     )
 }
 
