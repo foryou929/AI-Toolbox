483666f7d19ae666b6d7762b9c37c5a2e10e9a56
foryou929 <zxcvwert3457@gmail.com>
2024-08-02 21:46:02 +0900

    feat: add-drag-and-drop-upload-component

---
+++ src/components/DragDropUpload.jsx
@@ -0,0 +1,29 @@
+import { Flex, Image, Text } from '@chakra-ui/react';
+import { useDropzone } from 'react-dropzone';
+
+const DragDropUpload = ({ accept, onAccept }) => {
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        accept,
+        onDrop: (acceptedFiles) => {
+            onAccept(acceptedFiles[0]);
+        },
+    });
+
+    return (
+        <Flex
+            {...getRootProps()}
+            w='100%' h='100%' p={4}
+            direction='column' justify='center' align='center' cursor='pointer'
+        >
+            <input {...getInputProps()} />
+            <Image w={16} h={14} src='images/upload.png' />
+            {isDragActive ? (
+                <Text color='white' fontWeight='bold'>Drop the files here ...</Text>
+            ) : (
+                <Text color='white' fontWeight='bold'>Drag and drop a file or click to upload</Text>
+            )}
+        </Flex>
+    );
+};
+
+export default DragDropUpload;
