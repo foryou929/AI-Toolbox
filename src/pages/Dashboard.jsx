53dbf25a5bd4e6606b87c2a029b4144b7043f0d2
foryou929 <zxcvwert3457@gmail.com>
2024-08-02 21:46:43 +0900

    feat: update-dashboard-ui

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,18 +1,21 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Flex, Image } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
     const navigate = useNavigate();
 
     return (
-        <ButtonGroup>
-            <Button onClick={() => navigate('/lip-sync')}>
-                Lip sync
-            </Button>
-            <Button onClick={() => navigate('/live-portrait')}>
-                Live portrait
-            </Button>
-        </ButtonGroup>
+        <Flex h='100%' justify='center' align='center' bg='black'>
+            <ButtonGroup zIndex={5}>
+                <Button onClick={() => navigate('/lip-sync')}>
+                    Lip sync
+                </Button>
+                <Button onClick={() => navigate('/live-portrait')}>
+                    Live portrait
+                </Button>
+            </ButtonGroup>
+            <Image position='absolute' bottom={0} src="images/polygon.png" w='100%' />
+        </Flex>
     )
 }
 
