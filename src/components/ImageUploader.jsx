009f69813def437373dfcaf5835e4da7d87ee2ee
foryou929 <zxcvwert3457@gmail.com>
2024-08-04 01:43:08 +0900

    feat: add-image-uploder-component

---
+++ src/components/ImageUploader.jsx
@@ -0,0 +1,52 @@
+import { Box, Flex } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import DragDropUpload from "./DragDropUpload";
+import { CloseIcon } from "@chakra-ui/icons";
+
+const ImageUploader = ({ onChange, ...rest }) => {
+    const [src, setSRC] = useState('');
+    const [file, setFile] = useState();
+    const [isValid, setValid] = useState(false);
+
+    useEffect(() => {
+        if (file) {
+            setSRC(URL.createObjectURL(file));
+            setValid(true);
+        } else {
+            setValid(false);
+        }
+        onChange?.(file);
+    }, [file]);
+
+    const handleError = () => {
+        setValid(false);
+    }
+
+    return (
+        <Box {...rest}>
+            {isValid ? (
+                <Box w='100%' h='100%' position='relative'>
+                    <Flex
+                        position='absolute' right={2} top={2} zIndex={5}
+                        w={6} h={6}
+                        justify='center' align='center'
+                        bg='red.500' rounded='full' cursor='pointer'
+                        onClick={() => setFile(null)}
+                    >
+                        <CloseIcon fontSize='x-small' color='white' />
+                    </Flex>
+                    <img src={src} onError={handleError} />
+                </Box>
+            ) : (
+                <DragDropUpload
+                    accept={{
+                        'image/*': []
+                    }}
+                    onAccept={setFile}
+                />
+            )}
+        </Box>
+    )
+}
+
+export default ImageUploader;
