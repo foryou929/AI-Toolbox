eaf3b447db2027012cd4fc7a9f08420e114c9de1
foryou929 <zxcvwert3457@gmail.com>
2024-08-04 11:15:54 +0900

    feat: update-ui

--- src/pages/LivePortrait.jsx
+++ src/pages/LivePortrait.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Spinner } from "@chakra-ui/react";
+import { Box, Button, Flex, Spinner, Text } from "@chakra-ui/react";
 import { useState } from "react";
 import ImageUploader from "../components/ImageUploader";
 import VideoUploader from "../components/VideoUploader";
@@ -27,20 +27,34 @@ const LipSync = () => {
 
     return (
         <Box h='100%' bg='url("images/background.jpg")'>
-            <Flex mx='auto' p={{ base: 8, lg: 16 }} w='100%' maxW='1440px' h='100%' direction={{ base: 'column', lg: 'row' }} gap={4}>
-                <Flex w='100%' direction='column' gap={4}>
-                    <VideoUploader w='100%' aspectRatio={1.7778} bg='#FFF4' border='2px dashed #888' onChange={setVideo} />
-                    <ImageUploader w='100%' aspectRatio={1.7778} bg='#FFF4' border='2px dashed #888' onChange={setImage} />
-                </Flex>
-                <Flex w='100%' direction='column' gap={4}>
-                    <Box aspectRatio={1.7778} border='2px dashed #888' overflow='hidden'>
-                        <video src={src} controls style={{ width: '100%', height: '100%' }} />
-                    </Box>
-                    <Flex justify='right'>
-                        <Button isDisabled={!image || !video || isProcessing} onClick={handleProcess}>
-                            {isProcessing && <Spinner size='sm' mr={2} />}
-                            Process
-                        </Button>
+            <Flex mx='auto' p={{ base: 8, lg: 16 }} w='100%' maxW='1440px' h='100%' direction='column' gap={4}>
+                <Text fontSize='xx-large' fontWeight='bold' color='white' align='center' bgGradient='linear(to-r, #FF0080, #7928CA)' bgClip='text'>
+                    Live portrait
+                </Text>
+                <Flex w='100%' direction={{ base: 'column', lg: 'row' }} gap={4}>
+                    <Flex w='100%' direction='column' gap={4}>
+                        <Flex direction='column' gap={2}>
+                            <Text fontSize='large' fontWeight='bold' color='white'>Image</Text>
+                            <ImageUploader w='100%' aspectRatio={1.7778} bg='#FFF4' border='2px dashed #888' onChange={setImage} />
+                        </Flex>
+                        <Flex direction='column' gap={2}>
+                            <Text fontSize='large' fontWeight='bold' color='white'>Video</Text>
+                            <VideoUploader w='100%' aspectRatio={1.7778} bg='#FFF4' border='2px dashed #888' onChange={setVideo} />
+                        </Flex>
+                    </Flex>
+                    <Flex w='100%' direction='column' gap={4}>
+                        <Flex direction='column' gap={2}>
+                            <Text fontSize='large' fontWeight='bold' color='white'>Output</Text>
+                            <Box aspectRatio={1.7778} border='2px dashed #888' overflow='hidden'>
+                                <video src={src} controls style={{ width: '100%', height: '100%' }} />
+                            </Box>
+                        </Flex>
+                        <Flex justify='right'>
+                            <Button isDisabled={!image || !video || isProcessing} onClick={handleProcess}>
+                                {isProcessing && <Spinner size='sm' mr={2} />}
+                                Process
+                            </Button>
+                        </Flex>
                     </Flex>
                 </Flex>
             </Flex>
