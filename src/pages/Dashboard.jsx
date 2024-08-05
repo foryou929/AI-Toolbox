348cdd81596658da1086b55d59f31866c411aafa
foryou929 <zxcvwert3457@gmail.com>
2024-08-05 11:14:58 +0900

    feat: update-dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,31 +1,30 @@
-import { Button, ButtonGroup, Flex, Image, Text } from "@chakra-ui/react";
+import { Button, ButtonGroup, Flex, Text } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
     const navigate = useNavigate();
 
     return (
-        <Flex h='100%' direction='column' justify='center' align='center' gap={4} bg='black'>
-            <Text fontSize={{ base: 'xx-large', md: 'xxx-large' }} fontWeight='bold' bgGradient='linear(to-r, #FF1CF7, #00F0FF)' bgClip='text'>
+        <Flex h='100%' bg='url("images/polygon.png")' bgSize='cover' direction='column' justify='center' align='center' gap={4}>
+            <Text fontSize={{ base: '32px', md: '64px' }} fontWeight='bold' bgGradient='linear(to-r, #FF1CF7, #00F0FF)' bgClip='text'>
                 Lip Synchronization
             </Text>
-            <Text fontSize={{ base: 'xx-large', md: 'xxx-large' }} fontWeight='semibold' color='white'>
+            <Text fontSize={{ base: 'xx-large', md: 'xxx-large' }} fontWeight='bold'>
                 Real-Time High Quality
             </Text>
-            <Text w={{ base: '85%', md: '65%' }} fontSize={{ md: 'large' }} align='center' color='white'>
+            <Text w={{ base: '85%', md: '65%' }} fontSize={{ md: 'x-large' }} align='center'>
                 Unlock the future of animation with our state-of-the-art real-time lipsync technology, which delivers impeccably synchronized,
                 natural-looking lip movements for your virtual characters, enhancing their expressiveness and engagement
                 in any digital experience.
             </Text>
             <ButtonGroup zIndex={5}>
-                <Button size='lg' bgGradient='linear(to-b, #497CFF, #001664)' _hover={{ bg: 'unset' }} color='white' onClick={() => navigate('/lip-sync')}>
+                <Button size='lg' bgGradient='linear(to-b, #497CFF, #001664)' _hover={{ bg: 'unset' }} onClick={() => navigate('/lip-sync')}>
                     Lip sync
                 </Button>
-                <Button size='lg' bgGradient='linear(to-b, #497CFF, #001664)' _hover={{ bg: 'unset' }} color='white' onClick={() => navigate('/live-portrait')}>
+                <Button size='lg' bgGradient='linear(to-b, #497CFF, #001664)' _hover={{ bg: 'unset' }} onClick={() => navigate('/live-portrait')}>
                     Live portrait
                 </Button>
             </ButtonGroup>
-            <Image position='absolute' bottom={0} src="images/polygon.png" w='100%' />
         </Flex >
     )
 }
